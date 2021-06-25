adc006baf43c83f022a3aa952290b8ed4b4295c3
dladeira <dladeiratx@gmail.com>
2021-06-25 08:08:21 +0000

    Added HTTPS and Certificates
    Google Credentials now published (anyone is allowed to login with google)

--- index.js
+++ index.js
@@ -2,11 +2,18 @@ const app = require('./app.js')
 const io = require('./routes/socketRoutes.js')
 const mongoose = require('mongoose')
 const config = require('config')
+const https = require('https')
+const fs = require('fs')
+
+const serverOptions = {
+    key: fs.readFileSync('./certificates/privkey.pem'),
+    cert: fs.readFileSync('./certificates/fullchain.pem')
+}
 
 async function bootstrap() {
     await mongoose.connect(config.get("mongodb.connectionString"), config.get("mongodb.options"))
     console.log("Connected to MongoDB")
-    return app.listen(config.get("app.port"))
+    return https.createServer(serverOptions, app).listen(config.get("app.port"))
 }
 
 bootstrap().then(server => {
