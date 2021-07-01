a12b78dec4e7d926c4a1c78761223314bbb00c75
dladeira <dladeiratx@gmail.com>
2021-07-01 13:08:19 +0000

    Applied convention changes
    - Renamed filese to camelCase
    - Moved stylesheet link to cookieNotice.ejs
    - Moved consent box to the bottom
    - Applied fixes from codeFactor.io (instead of using onclick="" we now use JS to inject the event into the element)

---
+++ public/js/cookieNotice.js
@@ -0,0 +1,40 @@
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date()
+    d.setTime(d.getTime() + (exdays*24*60*60*1000))
+    let expires = "expires="+ d.toUTCString()
+    let sameSite = "SameSite=Lax"
+    document.cookie = cname + "=" + cvalue + ";" + sameSite + ";" + expires + ";path=/"
+}
+
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+        let c = ca[i]
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1)
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+}
+
+function hideCookieBar() {
+    document.getElementById("cookiebar").style.display = "none"
+}
+
+document.getElementById("acceptCookiesButton").onclick = () => {
+    setCookie("cookiesAccepted", true, 365)
+    hideCookieBar()
+}
+
+document.getElementById("declineCookiesButton").onclick = () => {
+    // i hope this is a legal way of preventing people from denying cookies
+    window.location.href = "about:blank"
+}
+
+if (getCookie("cookiesAccepted")) {
+    hideCookieBar()
+}
