3dcb04656417a86b4f0c3af7634bf436e60c4c2d
dladeira <dladeiratx@gmail.com>
2021-07-18 17:28:42 +0200

    Merged avatar and account information forms
    - Merged /avatar route into /updateAccount route
    - Created setAvatar setUsername and setDescription functions in account.route.js
    - Fixed style consistency in game.js
    - When image is uploaded form doesn't get auto submitted, but the image is displayed to the user

--- public/js/accountSettings.js
+++ public/js/accountSettings.js
@@ -1,18 +1,40 @@
-var usernameInput = document.getElementsByClassName("username-input")[0]
-var descriptionInput = document.getElementsByClassName("description-input")[0]
+var usernameInput = document.getElementsByClassName('username-input')[0]
+var descriptionInput = document.getElementsByClassName('description-input')[0]
+var avatarInput = document.getElementById('avatar-upload')
+var avatarImage = document.getElementById('js-avatar')
+var avatarForm = document.getElementById('js-avatar-form')
 
-var form = document.getElementById("form-js")
+var form = document.getElementById('form-js')
 var socket = io()
 var usernameExists = false
 
 form.onsubmit = formSubmitEvent
 
 usernameInput.oninput = () => {
+    if (usernameInput.value != username) {
+        usernameInput.classList.add('changedInput')
+    } else {
+        usernameInput.classList.remove('changedInput')
+    }
+
     socket.emit('usernameExists', { currentUsername: username, target: usernameInput.value })
 }
 
 descriptionInput.oninput = () => {
-    formSubmitEvent()
+    if (descriptionInput.value != description) {
+        descriptionInput.classList.add('changedInput')
+    } else {
+        descriptionInput.classList.remove('changedInput')
+    }
+    formSubmitEvent(undefined, false)
+}
+
+avatarInput.onchange = event => {
+    const [file] = avatarInput.files
+    if (file) {
+        avatarImage.src = URL.createObjectURL(file)
+        avatarImage.classList.add('changedInput')
+    }
 }
 
 socket.on('usernameExists', data => {
@@ -22,25 +44,24 @@ socket.on('usernameExists', data => {
     }
 })
 
-function formSubmitEvent(event) {
+function formSubmitEvent(event, displayUsernameAvailable) {
     var newUsername = usernameInput.value
-    if (!newUsername.match("^[a-zA-Z0-9]+$")) {
-        setUsernameStatus("Can only contain letters and numbers")
+    if (!newUsername.match('^[a-zA-Z0-9]+$')) {
+        setUsernameStatus('Can only contain letters and numbers')
         if (event) event.preventDefault()
         return false
     }
     if (newUsername.length < 4) {
-        setUsernameStatus("At least 4 characters")
+        setUsernameStatus('At least 4 characters')
         if (event) event.preventDefault()
         return false
     }
     if (newUsername.length > 20) {
-        setUsernameStatus("Less than 20 characters")
+        setUsernameStatus('Less than 20 characters')
         if (event) event.preventDefault()
         return false
     }
-
-    if (!event) setUsernameStatus(usernameExists ? "Username already exists" : "Username available")
+    if (!event && (displayUsernameAvailable != false)) setUsernameStatus(usernameExists ? 'Username already exists' : 'Username available')
     if (usernameExists) {
         if (event) event.preventDefault()
         return false
@@ -48,19 +69,19 @@ function formSubmitEvent(event) {
 
     var newDescription = descriptionInput.value
     if (newDescription.length > 30) {
-        setDescriptionStatus("Less than 30 characters")
+        setDescriptionStatus('Less than 30 characters')
         if (event) event.preventDefault()
         return false
     }
 
-    setDescriptionStatus("")
+    setDescriptionStatus('')
     return true
 }
 
 function setUsernameStatus(status) { // TODO: Support correct and error colors
-    document.getElementById("username-status").innerHTML = status;
+    document.getElementById('username-status').innerHTML = status
 }
 
 function setDescriptionStatus(status) { // TODO: Support correct and error colors
-    document.getElementById("description-status").innerHTML = status;
+    document.getElementById('description-status').innerHTML = status
 }
