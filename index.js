7f82e2efe8c14dda5c1bb8aca4ba7c3538dae31c
dladeira <dladeiratx@gmail.com>
2021-07-21 14:21:10 +0000

    Added node reverse proxy for production

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const app = require('./app.js')
 const io = require('./routes/socket.route.js')
 const mongoose = require('mongoose')
 const config = require('config')
-const http = require('http')
 const https = require('https')
 const fs = require('fs')
 
@@ -12,15 +11,19 @@ const serverOptions = {
 }
 
 async function bootstrap() {
-    let httpServer = http.createServer(app).listen(config.get('app.httpPort'))
-    let httpsServer = await https.createServer(serverOptions, app).listen(config.get('app.httpsPort'))
-    console.log(`Express listening on port ${config.get('app.httpPort')} (http) and ${config.get('app.httpsPort')} (https)`)
+    let server;
+    if (app.get('env') == 'production') {
+        server = await app.listen(config.get('app.port'))
+        console.log(`Express listening on port ${config.get('app.port')}`)
+    } else {
+        server = await https.createServer(serverOptions, app).listen(config.get('app.port'))
+        console.log(`Express listening on port ${config.get('app.httpPort')} (http) and ${config.get('app.httpsPort')} (https)`)
+    }
 
     await mongoose.connect(config.get('mongodb.connectionString'), config.get('mongodb.options'))
     console.log('Connected to MongoDB')
     
-    await io.attach(httpServer)
-    await io.attach(httpsServer)
+    await io.attach(server)
     console.log('Socket.io attached to server')
 }
 
