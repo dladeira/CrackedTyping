142cf5c35350169fb856dec085c0732e5d42b649
dladeira <dladeiratx@gmail.com>
2021-07-24 00:04:19 +0200

    Added text controls
    - Created new entry on control panel which allows you preform CRUD operations on texts

--- routes/control.route.js
+++ routes/control.route.js
@@ -26,7 +26,7 @@ router.post('/users', (req, res) => {
                 console.log(err)
                 return res.send('An error has occured, please try again later')
             }
-            
+
             if (mongoUser) {
                 mongoUser.admin = (req.body[mongoUser.username] == 1)
                 mongoUser.save()
@@ -34,6 +34,51 @@ router.post('/users', (req, res) => {
         })
     }
     res.redirect('/control')
+})
+
+router.get('/texts', (req, res) => {
+    Text.find({}, (err, texts) => {
+        if (err) {
+            console.log(err)
+            return res.send('An error has occured, please try again later')
+        }
+
+        res.render('control-texts.ejs', { texts: texts })
+    })
+})
+
+router.post('/texts', (req, res) => {
+    for (var text in req.body) {
+        if (text.includes('deletePassage'))
+            continue
+        Text.findOne({ _id: text}, (err, mongoText) => {
+            if (err) {
+                console.log(err)
+                return res.send('An error has occured, please try again later')
+            }
+
+            if (mongoText) {
+                if (req.body[mongoText._id + '-deletePassage']) {
+                    Text.deleteOne({ _id: mongoText._id}, (err) => {
+                        if (err)
+                            console.log(err)
+                    })
+                } else {
+                    mongoText.passage = req.body[mongoText._id]
+                    mongoText.save()
+                }
+            }
+        })
+    }
+    res.redirect('/control')
+})
+
+router.post('/texts/new', (req, res) => {
+    new Text({
+        passage: req.body.passage
+    }).save(() => {
+        res.redirect('/control')
+    })
 })
 
 module.exports = router
