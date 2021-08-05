257c19d595317aa647f7f1fe859ee8c38cfe08a0
dladeira <dladeiratx@gmail.com>
2021-08-05 09:26:17 +0200

    Added contribution control

--- models/contribution.model.js
+++ models/contribution.model.js
@@ -3,7 +3,11 @@ const mongoose = require('mongoose')
 const contributionSchema = new mongoose.Schema({
     contributionType: Number, // 0 = Text, 1 = Script
     contributionValue: String,
-    contributionAuthor: String // ID of the author
+    contributionAuthor: String, // ID of the author,
+    status: {// 0 = nothing, 1 = accepted, 2 = rejected
+        type: Number,
+        default: 0
+    }
 })
 
 const contributionModel = mongoose.model('Contribution', contributionSchema)
