8fabed59f0bf071100a0395a245bdf65758806a5
dladeira <dladeiratx@gmail.com>
2021-08-29 13:17:26 +0200

    Refactored code

--- public/js/cookieNotice.js
+++ public/js/cookieNotice.js
@@ -1,13 +1,13 @@
 function setCookie(cname, cvalue, exdays) {
     const d = new Date()
     d.setTime(d.getTime() + (exdays*24*60*60*1000))
-    let expires = "expires="+ d.toUTCString()
-    let sameSite = "SameSite=Lax"
-    document.cookie = cname + "=" + cvalue + ";" + sameSite + ";" + expires + ";path=/"
+    let expires = 'expires='+ d.toUTCString()
+    let sameSite = 'SameSite=Lax'
+    document.cookie = cname + '=' + cvalue + ';' + sameSite + ';' + expires + ';path=/'
 }
 
 function getCookie(cname) {
-    let name = cname + "=";
+    let name = cname + '=';
     let decodedCookie = decodeURIComponent(document.cookie);
     let ca = decodedCookie.split(';');
     for(let i = 0; i <ca.length; i++) {
@@ -22,20 +22,20 @@ function getCookie(cname) {
 }
 
 function hideCookieBar() {
-    document.getElementById("cookie-bar").style.display = "none"
+    document.getElementById('cookie-bar').style.display = 'none'
 }
 
-document.getElementById("cookie-accept").onclick = () => {
-    setCookie("cookiesAccepted", true, 365)
+document.getElementById('cookie-accept').onclick = () => {
+    setCookie('cookiesAccepted', true, 365)
     hideCookieBar()
 }
 
-document.getElementById("cookie-decline").onclick = () => {
+document.getElementById('cookie-decline').onclick = () => {
     // i hope this is a legal way of preventing people from denying cookies
-    window.location.href = "about:blank"
+    window.location.href = 'about:blank'
 }
 
 // Hide the bar if cookies are accepted
-if (getCookie("cookiesAccepted")) {
+if (getCookie('cookiesAccepted')) {
     hideCookieBar()
 }
